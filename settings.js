b7028acfe34d26a1fa80306b9c9e05738a1d4d17
EraPro <123062533+EraPro@users.noreply.github.com>
2023-02-26 09:01:09 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
+global.creator = 'ZeroGanzID'// yourname
 global.MONGO_DB_URI = "mongodb+srv://zeroapi:zeroapi@cluster0.qurl4ta.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
 global.your_email = "eraprivate01@gmail.com" //email

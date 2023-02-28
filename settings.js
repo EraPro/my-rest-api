bed2b8db4484b2de0025f28bc82f4f233e64e917
EraPro <eraprivate01@gmail.com>
2023-03-01 05:55:10 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 global.creator = 'ZeroGanzID'// yourname
-global.MONGO_DB_URI = "mongodb+srv://api:api@api.jbeqyx4.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
+global.MONGO_DB_URI = "mongodb+srv://api:api@app.00esm5a.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
 global.your_email = "privatez.api@gmail.com" //email
 global.email_password = "qhauhjotskldppka" //application password email
